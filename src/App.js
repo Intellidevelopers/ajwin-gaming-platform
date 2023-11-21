b050ea82f33c16443381f31f0fe7f8fd83856c83
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2023-11-21 09:25:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ajwin.sonicvibe.com.ng"
           target="_blank"
           rel="noopener noreferrer"
         >
